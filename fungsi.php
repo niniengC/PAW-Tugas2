9483791355372b4e63c40e5e71ee855502549625
niniengC <ninieng.choirunnisa@gmail.com>
2022-03-12 23:02:11 +0700

    menambahkan file fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,11 @@
+<?php 
+	function luasSegitiga($alas, $tinggi) {
+		$luas = ($alas * $tinggi) / 2;
+		echo "Luas Segitiga adalah " . $luas."<br>";
+	}
+
+	luasSegitiga(2,3);
+	luasSegitiga(5,6);
+	luasSegitiga(10,12);
+
+ ?>
