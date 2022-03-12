7ad6d513fa0f829b9c0c057a541c8c960705a922
niniengC <ninieng.choirunnisa@gmail.com>
2022-03-12 23:00:01 +0700

    menambahkan file variable.php

---
+++ variable.php
@@ -0,0 +1,14 @@
+<?php 
+	$a = 6;
+	$b = 3;
+	$c = 2;
+
+	$hasilJumlah = $a + $b + $c; //penjumlahan
+	$hasilKurang = $a - $b - $c; //pengurangan
+	$hasilKali = $a * $b * $c; //perkalian
+
+	echo "a = " . $a .", b = " . $b . ", c = " . $c . "<br><br>";
+	echo "hasilJumlah = a + b + c = " . $hasilJumlah . "<br>";
+	echo "hasilKurang = a - b - c = " . $hasilKurang . "<br>";
+	echo "hasilKali = a x b x c = " . $hasilKali;
+ ?>
